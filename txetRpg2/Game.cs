9a7153c2f74149ce36e3717c7e933e50048f067f
ManoftheStreet <zhijian6527@naver.com>
2023-07-12 21:53:37 +0900

    trpg2

---
+++ txetRpg2/Game.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace txetRpg2
+{
+    public enum GameMode
+    {
+        None,
+        Lobby,
+        Town,
+        Field
+    }
+    
+    
+    class Game
+    {
+        private GameMode mode = GameMode.Lobby;
+        private Player player = null;
+
+        public void Process()
+        {
+            switch (mode)
+            {
+                case GameMode.Lobby:
+                    ProcessLobby();
+                    break;
+                case GameMode.Town:
+                    ProcessTown();
+                    break;
+                case GameMode.Field:
+                    ProcessField();
+                    break;
+            }
+        }
+
+        private void ProcessLobby()
+        {
+            Console.WriteLine("직업을 선택하세요.");
+            Console.WriteLine("1. 기사");
+            Console.WriteLine("2. 궁수");
+            Console.WriteLine("3. 법사");
+
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    player = new Knight();
+                    Console.WriteLine("기사를 선택했습니다.");
+                    mode = GameMode.Town;
+                    break;
+                case "2":
+                    player = new Archer();
+                    Console.WriteLine("궁수를 선택했습니다.");
+                    mode = GameMode.Town;
+                    break;
+                case "3":
+                    player = new Mage();
+                    Console.WriteLine("법사를 선택했습니다.");
+                    mode = GameMode.Town;
+                    break;
+            }
+        }
+        private void ProcessTown()
+        {
+            Console.WriteLine("마을에 입장했습니다!");
+            Console.WriteLine("1. 필드로 가기");
+            Console.WriteLine("2. 로비로 돌아가기");
+
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    mode = GameMode.Field;
+                    break;
+                case "2":
+                    mode = GameMode.Lobby;
+                    break;
+            }
+        }
+        private void ProcessField()
+        {
+
+        }
+    }
+}
---
+++ txetRpg2/Player.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace txetRpg2
+{
+    public enum PlayerType //1. 열거하고
+    {
+        None = 0,
+        Knight = 1,
+        Archer = 2,
+        Mage = 3
+    }
+    internal class Player : Creature//2.정보 기입하고
+    {
+        protected PlayerType type = PlayerType.None;
+
+        protected Player(PlayerType type) : base(CreatureType.Player)
+        {
+            this.type = type;
+        }
+        public PlayerType GetPlayerType()
+        {
+            return type;
+        }
+    }
+
+    class Knight : Player//3.클래스 만들고
+    {
+        public Knight() : base(PlayerType.Knight)//4.생성자 만들어줌
+        {
+            SetInfo(100, 10);
+        }
+    }
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer)
+        {
+            SetInfo(75, 12);
+        }
+    }
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Mage)
+        {
+            SetInfo(50, 15);
+        }
+    }
+}
